29c71ebe0c909b832f11a15713302bca7a28eac7
Florian Fahrenberger <Floh.Fahrenberger@gmx.de>
2012-08-31 14:53:25 +0200

    Initial Commit

---
+++ Video Container Switcher/vcsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  vcsAppDelegate.h
+//  Video Container Switcher
+//
+//  Created by Florian Fahrenberger on 31.08.12.
+//  Copyright (c) 2012 Florian Fahrenberger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface vcsAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Video Container Switcher/vcsAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  vcsAppDelegate.m
+//  Video Container Switcher
+//
+//  Created by Florian Fahrenberger on 31.08.12.
+//  Copyright (c) 2012 Florian Fahrenberger. All rights reserved.
+//
+
+#import "vcsAppDelegate.h"
+
+@implementation vcsAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
